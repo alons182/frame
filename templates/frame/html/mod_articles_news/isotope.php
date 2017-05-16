b37ae720303ddc9c7f3efb1d60b3bd36e928b5cc
Alonso Chavarria Cubero <alons182@hotmail.com>
2017-05-15 21:26:02 -0600

    featured 3 4

--- templates/frame/html/mod_articles_news/isotope.php
+++ templates/frame/html/mod_articles_news/isotope.php
@@ -17,27 +17,26 @@ defined('_JEXEC') or die;
 
 <div class="newsflash<?php echo $moduleclass_sfx; ?>">
 	<div class="projects-container" data-nav-color="#fff">
-		<?php foreach ($list as $index => $item) : ?>
+		<?php foreach ($list as $index => $item) : 
+			$classFeatured = "default";
+		  ?>
 			
 			<?php if ( $item->id == 66 || $item->id == 58) :  ?>
-				<div class="projects-item featured-4" data-order="<?php echo $index ?>">
-				<?php require JModuleHelper::getLayoutPath('mod_articles_news', '_item'); ?>
-				</div>
-			<?php else if ( $item->id == 67) :  ?> 
-				<div class="projects-item featured-3" data-order="<?php echo $index ?>">
-				<?php require JModuleHelper::getLayoutPath('mod_articles_news', '_item'); ?>
-				</div>
-			<?php else if ( $item->id == 19 || $item->id == 14) :  ?> 
-				<div class="projects-item featured" data-order="<?php echo $index ?>">
-				<?php require JModuleHelper::getLayoutPath('mod_articles_news', '_item'); ?>
-				</div>
-			<?php else : ?>
-			 	<div class="projects-item default" data-order="<?php echo $index ?>">
-			 	<?php require JModuleHelper::getLayoutPath('mod_articles_news', '_item'); ?>
-				</div>
+				
+				<?php $classFeatured = "featured-4"; ?>
+				
+		    <?php endif; ?>
+			<?php if ( $item->id == 67) :  ?> 
+				<?php $classFeatured = "featured-3"; ?>
+			 <?php endif; ?>
+			<?php if ( $item->id == 19 || $item->id == 14) :  ?> 
+				<?php $classFeatured = "featured"; ?>
+			
 		    <?php endif; ?>
 			
-	
+				<div class="projects-item <?php echo $classFeatured ?>" data-order="<?php echo $index ?>">
+			 	<?php require JModuleHelper::getLayoutPath('mod_articles_news', '_item'); ?>
+				</div>
 
 		<?php endforeach; ?>
 	</div>
